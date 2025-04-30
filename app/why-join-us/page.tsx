a1103d499f463e6de13360f442f0acecaca58f2a
Aboulaye1263 <vieux.abdoulayedeme@gmail.com>
2025-04-30 16:32:18 +0000

    Update page.tsx

--- app/why-join-us/page.tsx
+++ app/why-join-us/page.tsx
@@ -208,18 +208,6 @@ export default function WhyJoinUs() {
               Postulez maintenant        
             </Link>
           </div>
-          {/* <div className="animate-[updown_2s_ease-in-out_infinite] relative z-10">
-            <svg
-              width="80"
-              height="52"
-              viewBox="0 0 24 24"
-              fill="#F3DFA2"
-              xmlns="http://www.w3.org/2000/svg"
-              className="rotate-3600 animate-[downward_2s_ease-in-out_infinite]"
-            >
-              <path d="M21 16v-2l-8-5V3.5c0-.83-.67-1.5-1.5-1.5S10 2.67 10 3.5V9l-8 5v2l8-2.5V19l-2 1.5V22l3.5-1 3.5 1v-1.5L13 19v-5.5l8 2.5z" />
-            </svg>
-          </div> */}
       </div>
     
 
